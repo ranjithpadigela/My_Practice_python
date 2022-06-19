7b4c727a023c4d29fb29bfe77b66ae328d2bc7d8
Padigela Ranjith <89335247+ranjithpadigela@users.noreply.github.com>
2022-06-19 22:23:27 +0530

    01 Ranjith
    This is python practice

---
+++ Demo_print_SSSP.py
@@ -0,0 +1,44 @@
+# I am the author of this program. 
+# I am P Ranjith 
+# Printing a few lines 
+# Printing the addition of 3 numbers.
+'''
+ comment line 1
+ comment line 2
+ comment line 3
+ comment line 4
+  comment line 5
+'''
+# print('My name  is Ranjith', 'Studied in NIET', sep='!',end=' ')  
+
+# User inputs  
+#  number1 = int(input("Enter the number 1 here:"))
+#  number2 = int(input("Enter the  number 2 here:")) 
+#  
+#  print('The  results  are:')
+#  Add = number1 + number2
+#  sub = number1 - number2
+#  div  = number1 /  number2
+#  mul = number1  *  number2
+#  exp = number1 ** number2
+#  
+#  print('Addition = ',Add)
+#  print('Subtraction = ',sub)
+#  print('Multiplication = ',mul)
+#  print('Division = ',div)
+#  print('Exponentiation = ',exp)
+#num1  = 7
+#num2  = 10
+#
+#if num1  > num2:
+#    diff  = num1  - num2
+#    print(num1,"-",num2, "=",diff)
+#else:
+#    diff = num2 - num1
+#    print(num2,"-",num1, "=",diff)
+
+str = 'MultipleWishes'
+for i in str[8:]:
+    print(i)
+
+search_for_string = 'I am practicing the python program and it is very easy to learn why because it has less and short syntax'
